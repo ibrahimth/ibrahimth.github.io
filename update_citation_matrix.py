f32666340d1b2b88b0534481dc6533b52be92eff
ibrahimth <i_ibrahim1@yahoo.com>
2025-02-25 02:11:56 +0300

    Update update_citation_matrix.py

--- update_citation_matrix.py
+++ update_citation_matrix.py
@@ -2,7 +2,7 @@ from scholarly import scholarly
 import os
 import subprocess
 import re
-
+import requests
 # Function to fetch citation count from Google Scholar
 def fetch_citation_data(scholar_url):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
