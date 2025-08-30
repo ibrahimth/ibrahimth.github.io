5fb9e25cc1e7fc46eac09016ab794ffcb997c1e1
ibrahimth <i_ibrahim1@yahoo.com>
2025-08-30 22:17:35 +0300

    Update vite.config.js

--- teaching/coe-292/vite.config.js
+++ teaching/coe-292/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // Final config for GitHub Pages deployment
 export default defineConfig({
-  base: '/teaching/coe-292/', // ensures assets load from this subpath
+  base: '/coe292-topic1/', // ensures assets load from this subpath
   plugins: [react()],
   resolve: {
     alias: {
